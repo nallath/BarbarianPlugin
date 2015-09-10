21e8a2f65c00d38231a4640ee5349e670a833194
Jaime van Kessel <nallath@gmail.com>
2015-09-10 17:20:28 +0200

    Initial commit

---
+++ BarbarianPlugin.py
@@ -0,0 +1,18 @@
+from UM.Extension import Extension
+from UM.Scene.Selection import Selection
+from UM.Math.Vector import Vector
+
+from UM.i18n import i18nCatalog
+i18n_catalog = i18nCatalog("BarbarianPlugin")
+
+class BarbarianPlugin(Extension):
+    def __init__(self):
+        super().__init__()
+        self.addMenuItem(i18n_catalog.i18n("Convert to metric"), self.convertToMetric)
+
+    def convertToMetric(self):
+        selected_nodes = Selection.getAllSelectedObjects()
+        for node in selected_nodes:
+            node.scale(Vector(25.4,25.4,25.4))
+
+
---
+++ __init__.py
@@ -0,0 +1,21 @@
+# Copyright (c) 2015 Jaime van Kessel
+# The BarbarianPlugin is released under the terms of the AGPLv3 or higher.
+
+from . import BarbarianPlugin
+from UM.i18n import i18nCatalog
+i18n_catalog = i18nCatalog("BarbarianPlugin")
+def getMetaData():
+    return {
+        "type": "extension",
+        "plugin":
+        {
+            "name": "Barbarian plugin",
+            "author": "Jaime van Kessel",
+            "version": "15.10",
+            "api": 2,
+            "description": i18n_catalog.i18nc("Description of plugin","Extension that allows to quickly convert models in inches to metric")
+        }
+    }
+
+def register(app):
+    return { "extension": BarbarianPlugin.BarbarianPlugin()}
