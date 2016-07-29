684c54f7b9d8d2711f15d8ac424aeaa21398c860
Jaime van Kessel <nallath@gmail.com>
2016-07-29 17:48:31 +0200

    Codestyle updates

--- BarbarianPlugin.py
+++ BarbarianPlugin.py
@@ -5,14 +5,14 @@ from UM.Math.Vector import Vector
 from UM.i18n import i18nCatalog
 i18n_catalog = i18nCatalog("BarbarianPlugin")
 
+
 class BarbarianPlugin(Extension):
     def __init__(self):
         super().__init__()
         self.addMenuItem(i18n_catalog.i18n("Convert to metric"), self.convertToMetric)
 
+    ##  Scales all selected objects by 25.4 (inch to mm)
     def convertToMetric(self):
         selected_nodes = Selection.getAllSelectedObjects()
         for node in selected_nodes:
-            node.scale(Vector(25.4,25.4,25.4))
-
-
+            node.scale(Vector(25.4, 25.4, 25.4))
--- __init__.py
+++ __init__.py
@@ -4,6 +4,8 @@
 from . import BarbarianPlugin
 from UM.i18n import i18nCatalog
 i18n_catalog = i18nCatalog("BarbarianPlugin")
+
+
 def getMetaData():
     return {
         "type": "extension",
@@ -13,9 +15,10 @@ def getMetaData():
             "author": "Jaime van Kessel",
             "version": "2.2",
             "api": 3,
-            "description": i18n_catalog.i18nc("Description of plugin","Extension that allows to quickly convert models in inches to metric")
+            "description": i18n_catalog.i18nc("Description of plugin", "Extension that allows to quickly convert models in inches to metric")
         }
     }
 
+
 def register(app):
-    return { "extension": BarbarianPlugin.BarbarianPlugin()}
+    return {"extension": BarbarianPlugin.BarbarianPlugin()}
